1c9a833b3981f992134103eab3121c696a1ab543
RJ Trujillo <CertifiedBlyndGuy@gmail.com>
2017-12-21 22:30:10 +0100

    build: Initial Uber bringup

--- core/product_config.mk
+++ core/product_config.mk
@@ -168,9 +168,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A GZOSP build needs only the GZOSP product makefiles.
-ifneq ($(GZOSP_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(GZOSP_BUILD)/gzosp.mk")
+# An Uber build needs only the Uber product makefiles.
+ifneq ($(UBER_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(UBER_BUILD)/uber.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -181,9 +181,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # GZOSP_BUILD
+endif # UBER_BUILD
 
-ifeq ($(GZOSP_BUILD),)
+ifeq ($(UBER_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
